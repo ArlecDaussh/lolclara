9319806578fee813e2fbcb517c7e701cf4094c9b
Clara Alves <claraabdelnur@gmail.com>
2020-03-29 22:35:32 -0300

    Attack range and attacking enemies

--- Assets/Scripts/NavGame/Core/AttackGameObject.cs
+++ Assets/Scripts/NavGame/Core/AttackGameObject.cs
@@ -12,6 +12,9 @@ namespace NavGame.Core
     {
 
         public OfenseStats ofenseStats;
+
+        public float attackRange = 4f;
+
         public string[] enemyLayers;
 
         [SerializeField]
@@ -34,6 +37,21 @@ namespace NavGame.Core
         protected virtual void Update()
         {
             DecreaseAttackCooldown();
+            UpdateAttack();
+        }
+
+        protected virtual void UpdateAttack()
+        {
+            if (enemiesToAttack.Count > 0)
+            {
+                agent.SetDestination(enemiesToAttack[0].gameObject.transform.position);
+                if (IsInRange(enemiesToAttack[0].gameObject.transform.position))
+                {
+                    agent.ResetPath();
+                    FaceObjectFrame(enemiesToAttack[0].gameObject.transform);
+                    AttackOnCooldown(enemiesToAttack[0]);
+                }
+            }
         }
 
         public void AttackOnCooldown(DamageableGameObject target)
@@ -83,6 +101,19 @@ namespace NavGame.Core
                DamageableGameObject obj = other.transform.parent.GetComponent<DamageableGameObject>();
                enemiesToAttack.Remove(obj);            
             }
+        }
+
+        public bool IsInRange(Vector3 point)
+        {
+            float distance = Vector3.Distance(transform.position, point);
+            return distance <= attackRange;
+        }
+
+        protected override void OnDrawGizmosSelected()
+        {
+            base.OnDrawGizmosSelected();
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, attackRange);
         }
     }
 }
