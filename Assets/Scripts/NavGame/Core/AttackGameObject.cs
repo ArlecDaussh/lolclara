3f2e206161cf8f752fb4957958d2c5ca469f3d50
Clara Alves <claraabdelnur@gmail.com>
2020-04-29 15:08:04 -0300

    Attack events

--- Assets/Scripts/NavGame/Core/AttackGameObject.cs
+++ Assets/Scripts/NavGame/Core/AttackGameObject.cs
@@ -14,6 +14,8 @@ namespace NavGame.Core
         public OfenseStats ofenseStats;
 
         public float attackRange = 4f;
+        public float attackDelay = 0.5f;
+        public Transform castTransform;
 
         public string[] enemyLayers;
 
@@ -27,12 +29,19 @@ namespace NavGame.Core
 
         LayerMask enemyMask;
 
-        public OnAttackHitEvent onAttackHit;
+        public OnAttackStartEvent onAttackStart;
+        public OnAttackCastEvent onAttackCast;
+        public OnAttackStrikeEvent onAttackStrike;
 
         protected virtual void Awake()
         {
             agent = GetComponent<NavMeshAgent>();
             enemyMask = LayerMask.GetMask(enemyLayers);
+
+            if (castTransform == null)
+            {
+                castTransform = transform;
+            }
         }
         protected virtual void Update()
         {
@@ -59,12 +68,31 @@ namespace NavGame.Core
             if (cooldown <= 0f)
             {
                 cooldown = 1f / ofenseStats.attackSpeed;
-                target.TakeDamage(ofenseStats.damage);
-                if (onAttackHit != null)
+                if (onAttackStart != null)
                 {
-                    onAttackHit(target.transform.position);
+                    onAttackStart();
+                }
+                StartCoroutine(AttackAfterDelay(target, attackDelay));
+
+            }
+        }
+
+        IEnumerator AttackAfterDelay(DamageableGameObject target, float delay)
+        {
+            yield return new WaitForSeconds(delay);
+            if (target != null)
+            {
+                if (onAttackCast != null)
+                {
+                    onAttackCast (castTransform.position);
                 }
 
+                target.TakeDamage(ofenseStats.damage);
+
+                if (onAttackStrike != null)
+                {
+                    onAttackStrike(target.damageTransform.position);
+                }
             }
         }
 
